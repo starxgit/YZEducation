1ae750d2f11d79e6eef14f0e707f9bbf0a53213e
shaoxin <shao-xin@qq.com>
2018-10-31 22:51:11 +0800

    新增了资源上传文件相关内容

---
+++ code/CommonFileServer/src/com/star/common/util/FileUtil.java
@@ -0,0 +1,35 @@
+package com.star.common.util;
+
+/**
+ * 文件处理的工具类 Created By ShaoXin On 2018-10-31
+ */
+public class FileUtil {
+
+    /**
+     * 上传文件的方法
+     * @parame uploadDir 上传目录
+     * @param File file 要上传的文件
+     * @param fileName 文件名
+     * @return fileUrl 文件地址
+     */
+    public static String upLoadFile() {
+        String fileName = "error";
+        
+        return fileName;
+    }
+    
+    /**
+     * 删除文件的方法 
+     */
+    public static void deleteFile(){
+        
+    }
+    
+    /**
+     * 断点下载文件的方法 
+     */
+    public static void downLoadFile(){
+        
+    }
+    
+}
