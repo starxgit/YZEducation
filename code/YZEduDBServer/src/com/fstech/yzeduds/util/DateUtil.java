8422fb254d9974e299a1928042bfc055e8aec3e8
shaoxin <shao-xin@qq.com>
2018-11-13 19:16:23 +0800

    增加了数据服务器框架，修改安卓学生端为本地调试地址

---
+++ code/YZEduDBServer/src/com/fstech/yzeduds/util/DateUtil.java
@@ -0,0 +1,26 @@
+package com.fstech.yzeduds.util;
+
+import java.util.Calendar;
+
+public class DateUtil {
+	
+	public static String mydatetime(){
+		String datetime=null;
+		 Calendar now = Calendar.getInstance();
+		datetime=buling(now.get(Calendar.YEAR))+"-"+
+				buling((now.get(Calendar.MONTH) + 1))+"-"+
+		 		buling(now.get(Calendar.DAY_OF_MONTH))+" "+
+		 		buling(now.get(Calendar.HOUR_OF_DAY))+":"+
+		 		buling(now.get(Calendar.MINUTE));
+		return datetime;
+	}
+	
+	public static String buling(int i){
+		String x=i+"";
+		if(x.length()==1){
+			x='0'+x;
+		}
+		return x;
+	}
+
+}
