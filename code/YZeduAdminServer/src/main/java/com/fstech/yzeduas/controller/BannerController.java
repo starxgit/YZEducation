23d48630543c3e5027b4fe5d21a6e68793f82ce3
shaoxin <shao-xin@qq.com>
2018-12-06 22:57:43 +0800

    新增了上传图片和Banner模块

--- code/YZeduAdminServer/src/main/java/com/fstech/yzeduas/controller/BannerController.java
+++ code/YZeduAdminServer/src/main/java/com/fstech/yzeduas/controller/BannerController.java
@@ -6,6 +6,7 @@ import com.fstech.yzeduas.util.SessionUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
